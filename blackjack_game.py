3dd332adfb343acecccb6ab69b4dbe8047a78865
andremcorona <corona.andrem@gmail.com>
2024-08-30 13:23:08 -0700

    Game can now be played continuously after implementing the ability to play another hand.

--- blackjack_game.py
+++ blackjack_game.py
@@ -4,59 +4,14 @@ from utils import *
 
 def main():
     deck = Deck()
-    player_hand = Hand()
-    dealer_hand = Hand()
-
-    # Deal initial cards
-    player_hand.add_card(deck.deal())
-    player_hand.add_card(deck.deal())
-    dealer_hand.add_card(deck.deal())
-    dealer_hand.add_card(deck.deal())
-
-    # Show player hands
-    print("Player's hand:", ', '.join(str(card) for card in player_hand.cards))
-    print("Player's hand value: ", player_hand.value)
+    gameOfBJ(deck)
     
-    # Show dealer up card
-    print("\nDealer's Up card: ", dealer_hand.cards[0])
+    # Ask user if they would like to keep playing
+    playAgain = input("Would you like to play again? (Yes/No): ")
 
-    # Check for BJ
-    playerBJ = checkBJ(player_hand)
-    dealerBJ = checkBJ(dealer_hand)
-
-    # Only allow game loop if there is no BJ
-    if playerBJ and not dealerBJ:
-        print("\nYou have Blackjack! :)")
-        gameOn = False
-    elif playerBJ and dealerBJ:
-        print("Player and Dealer both have Blackjack, it is a push!")
-        gameOn = False
-    else:
-        gameOn = True
-
-    # Game loop starts here
-    while gameOn:
-        # 1. Players Turn
-        playerBust = player_turn(player_hand, dealer_hand, deck)
-        if playerBust:
-            print("\nYou have busted. You Lose.")
-        # 2. After players turn and they didn't bust, it is dealers turn
-        print("\nNow for the dealer...")
-        dealerBust = dealer_turn(dealer_hand, deck)
-
-        # 3. Determine the winner and end the game
-        if dealerBust and not playerBust:
-            print("\nYou have won! :)")
-            gameOn = False
-        elif player_hand.value > dealer_hand.value and player_hand.value < 22:
-            print("\nYou have won! :)")
-            gameOn = False
-        elif player_hand.value == dealer_hand.value:
-            print("\nIt's a push!")
-            gameOn = False
-        else:
-            print("\nDealer Wins :(")
-            gameOn = False
+    if playAgain == "Yes":
+        checkForShuffle(deck)
+        gameOfBJ(deck)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -3,8 +3,7 @@ from hand import Hand
 
 def player_turn(player_hand, dealer_hand, deck):
     while player_hand.value < 22:
-        print("\nWould you like to Hit or Stand:")
-        playerChoice = input("")
+        playerChoice = input("\nWould you like to Hit or Stand: ")
         # If user hits
         if playerChoice == "Hit":
             player_hand.add_card(deck.deal())
@@ -42,3 +41,66 @@ def checkBJ(hand):
         return True
     else:
         return False
+    
+def dealHand(deck, player_hand, dealer_hand):
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+    
+def checkForShuffle(deck):
+    if deck.cards == 0:
+        deck.shuffle
+
+def gameOfBJ(deck):
+    player_hand = Hand()
+    dealer_hand = Hand()
+
+    # Deal initial cards
+    dealHand(deck, player_hand, dealer_hand)
+
+    # Show player hands
+    print("\nPlayer's hand:", ', '.join(str(card) for card in player_hand.cards))
+    print("Player's hand value: ", player_hand.value)
+    
+    # Show dealer up card
+    print("\nDealer's Up card: ", dealer_hand.cards[0])
+
+    # Check for BJ
+    playerBJ = checkBJ(player_hand)
+    dealerBJ = checkBJ(dealer_hand)
+
+    # Only allow game loop if there is no BJ
+    if playerBJ and not dealerBJ:
+        print("\nYou have Blackjack! :)")
+        gameOn = False
+    elif playerBJ and dealerBJ:
+        print("Player and Dealer both have Blackjack, it is a push!")
+        gameOn = False
+    else:
+        gameOn = True
+
+    # Game loop starts here
+    while gameOn:
+        # 1. Players Turn
+        playerBust = player_turn(player_hand, dealer_hand, deck)
+        if playerBust:
+            print("\nYou have busted. You Lose.")
+            gameOn = False
+        # 2. After players turn and they didn't bust, it is dealers turn
+        print("\nNow for the dealer...")
+        dealerBust = dealer_turn(dealer_hand, deck)
+
+        # 3. Determine the winner and end the game
+        if dealerBust and not playerBust:
+            print("\nYou have won! :)")
+            gameOn = False
+        elif player_hand.value > dealer_hand.value and player_hand.value < 22:
+            print("\nYou have won! :)")
+            gameOn = False
+        elif player_hand.value == dealer_hand.value:
+            print("\nIt's a push!")
+            gameOn = False
+        else:
+            print("\nDealer Wins :(")
+            gameOn = False
